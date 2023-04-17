d61dbb5808fe9483ed92f81bbdbeb0490a3cabcc
tejaskatkade <tejaskatkade44@gmail.com>
2023-04-17 22:44:03 +0530

    17-04-2023

---
+++ codes/array/arrDeclaration.java
@@ -0,0 +1,17 @@
+
+
+class ArrayDemo{
+
+	int a =10; // Global variable (instance variable);
+	
+	public static void main(String[] args){
+	
+		int b = 20;  //Local variable
+
+		System.out.println(b);  
+		// System.out.println(a);  // error: non-static variable a cannot be referenced from a static context
+
+
+	}
+	
+}
---
+++ codes/array/arrDeclaration1.java
@@ -0,0 +1,27 @@
+// Array
+
+class ArrayDemo{
+
+	public static void main(String[] args){
+	
+		//int arr[5];   // error;
+
+		int arr1[];   // NO error;
+			     // Declaration;
+
+		int arr3[] = new int [5];
+	//	int arr[] = new int [5]{1,2,3,4,5};   // error: array creation with both dimension expression and initialization is illegal
+	
+
+		int arr2[] = new int []{1,2,3,4,5};
+		
+		int arr4[]= {10,20,30,40,50,};
+
+		// Above both lines are same 
+		// array gets memory at heap section.
+
+	
+
+
+	}
+}
